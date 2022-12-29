23c5b276d3b7d7b9b2e056cf78e423957b87e42c
MantasKub <mkubiliunas@gmail.com>
2022-12-29 21:51:22 +0200

    Updated

---
+++ landingpage/assets/js/script.js
@@ -0,0 +1,35 @@
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 0) {
+    document.querySelector("header").classList.add("scrolling");
+  } else {
+    document.querySelector("header").classList.remove("scrolling");
+  }
+});
+
+document.querySelectorAll(".tabs-nav li").forEach((el) => {
+  const target = el.dataset.targer;
+
+  el.addEventListener("click", () => {
+    document.querySelectorAll(".tabs .content").forEach((section) => {
+      section.style.display = "none";
+    });
+    document.getElementById(target).style.display = "flex";
+  });
+});
+
+document.querySelectorAll("section.pricing .pricing-buttons button").forEach((el) => {
+  el.addEventListener("click", (event) => {
+    document.querySelector(
+      "section.pricing [data-plan='basic'] .amount"
+    ).textContent = el.dataset.basic;
+    document.querySelector(
+      "section.pricing [data-plan='standard'] .amount"
+    ).textContent = el.dataset.standard;
+
+    document.querySelectorAll("section.pricing .pricing-buttons button")
+      .forEach((button) => {
+        button.classList.remove('active');
+      })
+    event.target.classList.add('active');
+  });
+});
